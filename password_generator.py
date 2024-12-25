e65ed35d87b6b9b731be8a6605cf82c95873f512
Vesel4ak <gushchinkv@yandex.ru>
2024-12-25 17:44:32 +0300

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,5 +1,9 @@
+#import directories
+
 from random import choice
 from time import sleep
+
+#main function
 
 def generate_password():
     variants =["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
@@ -8,8 +12,14 @@ def generate_password():
     sleep(1.5)
     password =""
     for i in range(16):
+
+#generating
+        
         numbers = choice(variants)
         password +=numbers
     print(f"Твой пароль: {password}")
+
+#chekig for launch
+
 if __name__ =="__main__":
     generate_password()
